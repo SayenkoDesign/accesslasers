b39ecc61564603e152c547f52994e45bec201f43
Kyle Rumble <kylerumble@gmail.com>
2019-08-07 15:36:05 -0700

    single post mailchimp form

--- wp-content/themes/sd-accesslasers/template-parts/content-post.php
+++ wp-content/themes/sd-accesslasers/template-parts/content-post.php
@@ -33,6 +33,14 @@
                 _s_get_addtoany_share_icons(),
                 $navigation  
               );
+              
+        
+        $form_id = absint( 3 ); 
+        $form = GFAPI::get_form( $form_id );
+        if( false !== $form ) {
+           printf( '<h3 class="h5">Get the latest stories delivered straight to your inbox</h5>%s',  do_shortcode( sprintf( '[gravityform id="%s" title="false" description="false" ajax="false"]', $form_id ) ) );
+        }
+            
                           
         ?>           
 	</footer><!-- .entry-footer -->
